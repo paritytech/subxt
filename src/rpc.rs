013b8843f2a2d5b7970df1007a1423e5de602b70
Maciej Hirsz <hello@maciej.codes>
2019-12-19 17:52:09 +0100

    feat: Introduce Client::finalized_head

--- src/rpc.rs
+++ src/rpc.rs
@@ -168,6 +168,13 @@ impl<T: System> Rpc<T> {
             })
     }
 
+    /// Get a block hash of the latest finalized block
+    pub fn finalized_head(&self) -> impl Future<Item = T::Hash, Error = Error> {
+        self.chain
+            .finalized_head()
+            .map_err(Into::into)
+    }
+
     /// Get a Block
     pub fn block(
         &self,
