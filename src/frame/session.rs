0084f875756e08ae529e9766a4519fec65f1b01f
Demi M. Obenour <demiobenour@gmail.com>
2020-09-11 16:20:08 -0400

    Fix header

--- src/frame/session.rs
+++ src/frame/session.rs
@@ -1,19 +1,18 @@
 // Copyright 2019-2020 Parity Technologies (UK) Ltd.
 // This file is part of substrate-subxt.
 //
-// substrate-subxt is free software: you can redistribute it and/or modify
+// subxt is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// substrate-subxt is distributed in the hope that it will be useful,
+// subxt is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with substrate-subxt.  If not, see <http://www.gnu.org/licenses/>.
-// Copyright 2019-2020 Parity Technologies (UK) Ltd.
 
 //! Session support
 use crate::frame::system::{
