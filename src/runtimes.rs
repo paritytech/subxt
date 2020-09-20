d659e91c882ad402ff35cb0064ad033be95cd8e0
Demi M. Obenour <demiobenour@gmail.com>
2020-09-20 19:07:05 -0400

    Remove spurious diff

--- src/runtimes.rs
+++ src/runtimes.rs
@@ -111,7 +111,6 @@ impl_opaque_keys! {
 }
 
 use crate::{
-    contracts::Contracts,
     extrinsic::{
         DefaultExtra,
         SignedExtra,
@@ -121,6 +120,7 @@ use crate::{
             AccountData,
             Balances,
         },
+        contracts::Contracts,
         session::Session,
         staking::Staking,
         sudo::Sudo,
