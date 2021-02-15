11d2b29ea10c38c6aeeafa28e858c4bfe0de1396
Andrew Jones <ascjones@gmail.com>
2021-02-15 16:01:11 +0000

    Use MultiAddress for test runtime

--- src/runtimes.rs
+++ src/runtimes.rs
@@ -225,7 +225,7 @@ impl System for NodeTemplateRuntime {
     type Hash = sp_core::H256;
     type Hashing = BlakeTwo256;
     type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
-    type Address = Self::AccountId;
+    type Address = sp_runtime::MultiAddress<Self::AccountId, u32>;
     type Header = Header<Self::BlockNumber, BlakeTwo256>;
     type Extrinsic = OpaqueExtrinsic;
     type AccountData = AccountData<<Self as Balances>::Balance>;
