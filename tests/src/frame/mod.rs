b95ce3d4859782c5b82648f3f56df7022e1c146f
Andrew Jones <ascjones@gmail.com>
2021-10-06 12:27:59 +0100

    Restore sudo tests

--- tests/src/frame/mod.rs
+++ tests/src/frame/mod.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with substrate-subxt.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Test interactions with some built-in FRAME pallets.
+
 mod balances;
 mod contracts;
 mod staking;
+mod sudo;
