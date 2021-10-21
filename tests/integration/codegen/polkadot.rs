adf8ebd809190c2d9ae9760dc61664d1a4c6c1b4
Andrew Jones <ascjones@gmail.com>
2021-10-21 17:46:56 +0100

    Regenerate polkadot api with bitvec fix

--- tests/integration/codegen/polkadot.rs
+++ tests/integration/codegen/polkadot.rs
@@ -15510,7 +15510,7 @@ pub mod api {
                     Debug, Eq, PartialEq, :: codec :: Encode, :: codec :: Decode,
                 )]
                 pub struct AvailabilityBitfield(
-                    pub  runtime_types::bitvec::vec::BitVec<
+                    pub  ::subxt::bitvec::vec::BitVec<
                         runtime_types::bitvec::order::Lsb0,
                         u8,
                     >,
@@ -15525,7 +15525,7 @@ pub mod api {
                         >,
                     pub validity_votes:
                         Vec<runtime_types::polkadot_primitives::v0::ValidityAttestation>,
-                    pub validator_indices: runtime_types::bitvec::vec::BitVec<
+                    pub validator_indices: ::subxt::bitvec::vec::BitVec<
                         runtime_types::bitvec::order::Lsb0,
                         u8,
                     >,
@@ -16469,11 +16469,11 @@ pub mod api {
                     pub hash: runtime_types::polkadot_core_primitives::CandidateHash,
                     pub descriptor:
                         runtime_types::polkadot_primitives::v1::CandidateDescriptor<_0>,
-                    pub availability_votes: runtime_types::bitvec::vec::BitVec<
+                    pub availability_votes: ::subxt::bitvec::vec::BitVec<
                         runtime_types::bitvec::order::Lsb0,
                         u8,
                     >,
-                    pub backers: runtime_types::bitvec::vec::BitVec<
+                    pub backers: ::subxt::bitvec::vec::BitVec<
                         runtime_types::bitvec::order::Lsb0,
                         u8,
                     >,
