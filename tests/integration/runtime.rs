43f59e41ce1eb327623c555fb9bf76a9ec5da296
Andrew Jones <ascjones@gmail.com>
2021-10-28 11:39:29 +0200

    Add generated type derives for test runtime api

--- tests/integration/runtime.rs
+++ tests/integration/runtime.rs
@@ -14,5 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with subxt.  If not, see <http://www.gnu.org/licenses/>.
 
-#[subxt::subxt(runtime_metadata_path = "tests/integration/node_runtime.scale")]
+#[subxt::subxt(
+    runtime_metadata_path = "tests/integration/node_runtime.scale",
+    generated_type_derives = "Debug, Eq, PartialEq",
+)]
 pub mod node_runtime {}
