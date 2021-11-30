1a3b5a9fe19843bc9f517bf2dc31c5910171ab72
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-11-30 22:34:51 +0100

    cargo fmt

--- client/src/tests.rs
+++ client/src/tests.rs
@@ -1,7 +1,19 @@
-use node_cli::service::NewFullBase;
-use sp_keyring::AccountKeyring;
-use subxt::{ClientBuilder, PairSigner};
-use test_runtime::node_runtime::{self, system, DefaultConfig}
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of subxt.
+//
+// subxt is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// subxt is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with subxt.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::{
     DatabaseSource,
     KeystoreConfig,
@@ -10,7 +22,18 @@ use crate::{
     SubxtClientConfig,
     WasmExecutionMethod,
 };
+use node_cli::service::NewFullBase;
+use sp_keyring::AccountKeyring;
+use subxt::{
+    ClientBuilder,
+    PairSigner,
+};
 use tempdir::TempDir;
+use test_runtime::node_runtime::{
+    self,
+    system,
+    DefaultConfig,
+};
 
 #[async_std::test]
 pub async fn test_embedded_client() {
