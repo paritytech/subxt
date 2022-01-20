d7e5914e39d832584a7ba132b94b701d7009fc8b
James Wilson <james@jsdw.me>
2022-01-20 13:57:38 +0000

    silcnce clippy in example

--- examples/custom_type_derives.rs
+++ examples/custom_type_derives.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with subxt.  If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(clippy::redundant_clone)]
+
 #[subxt::subxt(
     runtime_metadata_path = "examples/polkadot_metadata.scale",
     // We can add (certain) custom derives to the generated types by providing
