bfc7fe195da2e7559df4cbbd28232ae1a5524209
Alexandru Vasile <alexandru.vasile@parity.io>
2022-02-14 18:17:48 +0200

    Revert "codegen: Update polkadot.rs with copyright"
    This reverts commit 2970d0573dc0b11d01072b270a525ad497992ddf.
    
    Revert "cli: Fix polkadot.rs license check"
    
    This reverts commit 6fe8818582ae39669c059c1ed0424b6606620295.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -167,26 +167,6 @@ fn codegen<I: Input>(
     derives.append(p.into_iter());
 
     let runtime_api = generator.generate_runtime(item_mod, derives);
-    println!(
-        "
-        // Copyright 2019-2022 Parity Technologies (UK) Ltd.
-        // This file is part of subxt.
-        //
-        // subxt is free software: you can redistribute it and/or modify
-        // it under the terms of the GNU General Public License as published by
-        // the Free Software Foundation, either version 3 of the License, or
-        // (at your option) any later version.
-        //
-        // subxt is distributed in the hope that it will be useful,
-        // but WITHOUT ANY WARRANTY; without even the implied warranty of
-        // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        // GNU General Public License for more details.
-        //
-        // You should have received a copy of the GNU General Public License
-        // along with subxt.  If not, see <http://www.gnu.org/licenses/>.
-
-        {}",
-        runtime_api
-    );
+    println!("{}", runtime_api);
     Ok(())
 }
