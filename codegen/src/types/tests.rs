27735a731215bedfe722ffac288b74595757cdd1
Andrew Jones <ascjones@gmail.com>
2022-02-23 12:45:58 +0000

    More bitvec updates

--- codegen/src/types/tests.rs
+++ codegen/src/types/tests.rs
@@ -710,8 +710,8 @@ fn generate_bitvec() {
                 use super::root;
                 #[derive(::subxt::codec::Encode, ::subxt::codec::Decode, Debug)]
                 pub struct S {
-                    pub lsb: ::subxt::bitvec::vec::BitVec<root::bitvec::order::Lsb0, ::core::primitive::u8>,
-                    pub msb: ::subxt::bitvec::vec::BitVec<root::bitvec::order::Msb0, ::core::primitive::u16>,
+                    pub lsb: ::subxt::bitvec::vec::BitVec<::core::primitive::u8, root::bitvec::order::Lsb0>,
+                    pub msb: ::subxt::bitvec::vec::BitVec<::core::primitive::u16, root::bitvec::order::Msb0>,
                 }
             }
         }
