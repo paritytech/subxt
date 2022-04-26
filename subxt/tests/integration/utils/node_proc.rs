e355ec66013bd60e1976a69ea4fc10838ce80a10
Alexandru Vasile <alexandru.vasile@parity.io>
2022-04-26 18:27:48 +0300

    subxt/tests: Add ws address to `TestNodeProcess`
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- subxt/tests/integration/utils/node_proc.rs
+++ subxt/tests/integration/utils/node_proc.rs
@@ -37,6 +37,7 @@ use subxt::{
 pub struct TestNodeProcess<R: Config> {
     proc: process::Child,
     client: Client<R>,
+    ws_url: String,
 }
 
 impl<R> Drop for TestNodeProcess<R>
@@ -74,6 +75,11 @@ where
     /// Returns the subxt client connected to the running node.
     pub fn client(&self) -> &Client<R> {
         &self.client
+    }
+
+    /// Returns the address to which the client is connected.
+    pub fn ws_url(&self) -> &str {
+        &self.ws_url
     }
 }
 
@@ -137,7 +143,13 @@ impl TestNodeProcessBuilder {
         // Connect to the node with a subxt client:
         let client = ClientBuilder::new().set_url(ws_url.clone()).build().await;
         match client {
-            Ok(client) => Ok(TestNodeProcess { proc, client }),
+            Ok(client) => {
+                Ok(TestNodeProcess {
+                    proc,
+                    client,
+                    ws_url,
+                })
+            }
             Err(err) => {
                 let err = format!("Failed to connect to node rpc at {}: {}", ws_url, err);
                 log::error!("{}", err);
