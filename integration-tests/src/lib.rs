e857e7e88fc803ff4428b8d62454a80dd31b2536
Alexandru Vasile <alexandru.vasile@parity.io>
2022-04-29 19:33:19 +0300

    integration-tests: Move tests content under src
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -13,3 +13,24 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with subxt.  If not, see <http://www.gnu.org/licenses/>.
+
+#[cfg(test)]
+mod codegen;
+#[cfg(test)]
+mod utils;
+
+#[cfg(test)]
+mod client;
+#[cfg(test)]
+mod events;
+#[cfg(test)]
+mod frame;
+#[cfg(test)]
+mod metadata;
+#[cfg(test)]
+mod storage;
+
+#[cfg(test)]
+use test_runtime::node_runtime;
+#[cfg(test)]
+use utils::*;
