7336388161ed9a0a07348a058b0e9f5bccc77d29
Alexandru Vasile <alexandru.vasile@parity.io>
2022-05-19 14:18:17 +0300

    Add documentation for the `extrinsic` module
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- subxt/src/extrinsic/mod.rs
+++ subxt/src/extrinsic/mod.rs
@@ -15,6 +15,28 @@
 // along with subxt.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Create signed or unsigned extrinsics.
+//!
+//! This modules exposes the extrinsic's parameters and the ability to sign an extrinsic.
+//!
+//!
+//! An extrinsic can be submitted with an "signed extra" and "additional" parameters for
+//! further customization. The trait [ExtrinsicParams] is at the core of an extrinsic parameter.
+//!
+//!
+//! The structure [BaseExtrinsicParams] is a default implementation of the trait for the
+//! Polkadot/Substrate chains.
+//! For this implementation:
+//! - "signed extra" contains:
+//!     - sp_runtime::Era: This is utilized to determine the longevity of a transaction.
+//!     - nonce: Account index (aka nonce) that stores the number of previous transactions
+//!         associated with a sender account. This is utilized to avoid replay attacks.
+//!     - tip: A tip payment for including the transaction in the block.
+//! - "additional" contains:
+//!     - spec_version: The version of the runtime specification of the node.
+//!     - transaction_version: The version of the extrinsic interface. This allows hardware
+//!         wallets to know which transactions can be safely signed.
+//!     - genesis_hash: The hash of the Genesis block.
+//!     - mortality_checkpoint: The block hash after which the transaction becomes valid.
 
 mod params;
 mod signer;
