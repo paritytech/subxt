03f23b135971de81a126260a03161d2fbdb739c0
James Wilson <james@jsdw.me>
2022-06-17 14:37:35 +0100

    tracing_subscriber now

--- examples/examples/rpc_call_subscribe_blocks.rs
+++ examples/examples/rpc_call_subscribe_blocks.rs
@@ -38,7 +38,7 @@ pub mod polkadot {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    tracing_subscriber::fmt::init();
 
     let api = ClientBuilder::new()
         .set_url("wss://rpc.polkadot.io:443")
