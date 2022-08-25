ba4966666861dfee1478f1c5bbf5abd698395881
Michael Müller <mich@elmueller.net>
2022-08-25 11:06:27 +0200

    Update subxt/src/rpc.rs
    Co-authored-by: Andrew Jones <ascjones@gmail.com>

--- subxt/src/rpc.rs
+++ subxt/src/rpc.rs
@@ -176,6 +176,7 @@ impl From<U256> for NumberOrHex {
 }
 
 /// An error type that signals an out-of-range conversion attempt.
+#[derive(thiserror::Error)]
 pub struct TryFromIntError(pub(crate) ());
 
 impl TryFrom<NumberOrHex> for u32 {
