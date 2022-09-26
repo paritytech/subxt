9dcffdaa57a42228769e3cbc34f33bb430e4485c
James Wilson <james@jsdw.me>
2022-09-26 11:50:07 +0100

    add license headers

--- cli/src/commands/codegen.rs
+++ cli/src/commands/codegen.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2022 Parity Technologies (UK) Ltd.
+// This file is dual-licensed as Apache-2.0 or GPL-3.0.
+// see LICENSE for license details.
+
 use color_eyre::eyre;
 use frame_metadata::RuntimeMetadataPrefixed;
 use jsonrpsee::client_transport::ws::Uri;
