738cd8272aeb285f90c12178e903fc6e514a04a7
Alexandru Vasile <alexandru.vasile@parity.io>
2022-09-29 14:56:19 +0300

    rpc: Fill in any missing finalized blocks
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- subxt/src/rpc/rpc.rs
+++ subxt/src/rpc/rpc.rs
@@ -56,6 +56,12 @@ use codec::{
     Encode,
 };
 use frame_metadata::RuntimeMetadataPrefixed;
+use futures::{
+    future::Either,
+    stream,
+    Stream,
+    StreamExt,
+};
 use serde::{
     Deserialize,
     Serialize,
@@ -74,6 +80,7 @@ use sp_runtime::{
         Block,
         SignedBlock,
     },
+    traits::Header,
     ApplyExtrinsicResult,
 };
 use std::collections::HashMap;
@@ -567,6 +574,97 @@ impl<T: Config> Rpc<T> {
             )
             .await?;
         Ok(subscription)
+    }
+
+    /// Subscribe to finalized blocks and ensure that all missing blocks are filled.
+    ///
+    /// The Substrate's RPC does not guarantee that all finalized blocks are provided.
+    /// This function ensures that all missed blocks are fetched and returned in order.
+    ///
+    /// # Example
+    ///
+    /// The following is a possible scenario where Substrate does not generate an event
+    /// for the second block.
+    ///
+    /// ```sh
+    /// substrate      | Block A | --- | Block B | --- | Block C |
+    /// client         | Block A | ---             --- | Block C |
+    /// ```
+    ///
+    /// This function mitigates this by filling the gaps of missing blocks.
+    ///
+    /// ```sh
+    /// substrate      | Block A | --- | Block B | --- | Block C |
+    /// client         | Block A | --- | Block B | --- | Block C |
+    ///                                ^^^ filled
+    /// ```
+    pub async fn subscribe_finalized_blocks_filled(
+        &self,
+    ) -> Result<impl Stream<Item = Result<T::Header, Error>> + Send + '_, Error> {
+        // Fetch the last finalised block details immediately, so that we'll get
+        // all blocks after this one.
+        let last_finalized_block_hash = self.finalized_head().await?;
+        let last_finalized_block_num = self
+            .header(Some(last_finalized_block_hash))
+            .await?
+            .map(|h| (*h.number()).into());
+
+        let sub = self.subscribe_finalized_blocks().await?;
+
+        // Adjust the subscription stream to fill in any missing blocks.
+        Ok(Rpc::subscribe_to_block_headers_filling_in_gaps(
+            self.clone(),
+            last_finalized_block_num,
+            sub,
+        ))
+    }
+
+    /// Note: This is exposed for testing but is not considered stable and may change
+    /// without notice in a patch release.
+    #[doc(hidden)]
+    pub fn subscribe_to_block_headers_filling_in_gaps<S, E>(
+        client: Rpc<T>,
+        mut last_block_num: Option<u64>,
+        sub: S,
+    ) -> impl Stream<Item = Result<T::Header, Error>> + Send
+    where
+        S: Stream<Item = Result<T::Header, E>> + Send,
+        E: Into<Error> + Send + 'static,
+    {
+        sub.flat_map(move |s| {
+            let rpc = client.clone();
+
+            // Get the header, or return a stream containing just the error.
+            let header = match s {
+                Ok(header) => header,
+                Err(e) => return Either::Left(stream::once(async { Err(e.into()) })),
+            };
+
+            // We want all previous details up to, but not including this current block num.
+            let end_block_num = (*header.number()).into();
+
+            // This is one after the last block we returned details for last time.
+            let start_block_num = last_block_num.map(|n| n + 1).unwrap_or(end_block_num);
+
+            // Iterate over all of the previous blocks we need headers for, ignoring the current block
+            // (which we already have the header info for):
+            let previous_headers = stream::iter(start_block_num..end_block_num)
+                .then(move |n| {
+                    let rpc = rpc.clone();
+                    async move {
+                        let hash = rpc.block_hash(Some(n.into())).await?;
+                        let header = rpc.header(hash).await?;
+                        Ok::<_, Error>(header)
+                    }
+                })
+                .filter_map(|h| async { h.transpose() });
+
+            // On the next iteration, we'll get details starting just after this end block.
+            last_block_num = Some(end_block_num);
+
+            // Return a combination of any previous headers plus the new header.
+            Either::Right(previous_headers.chain(stream::once(async { Ok(header) })))
+        })
     }
 
     /// Subscribe to runtime version updates that produce changes in the metadata.
