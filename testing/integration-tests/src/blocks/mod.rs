a86237a5f6dfbf38f8764a39194cd5bdfdbfeed1
Alexandru Vasile <alexandru.vasile@parity.io>
2022-09-30 17:07:16 +0300

    tests: Add tests for `subscribe_headers`
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- testing/integration-tests/src/blocks/mod.rs
+++ testing/integration-tests/src/blocks/mod.rs
@@ -5,6 +5,25 @@
 use crate::test_context;
 use futures::StreamExt;
 
+// Check that we can subscribe to non-finalized blocks.
+#[tokio::test]
+async fn non_finalized_headers_subscription() -> Result<(), subxt::Error> {
+    let ctx = test_context().await;
+    let api = ctx.client();
+
+    let mut sub = api.blocks().subscribe_headers().await?;
+
+    // Wait for the next set of headers, and check that the
+    // associated block hash is the one we just finalized.
+    // (this can be a bit slow as we have to wait for finalization)
+    let header = sub.next().await.unwrap()?;
+    let block_hash = header.hash();
+    let current_block_hash = api.rpc().block_hash(None).await?.unwrap();
+
+    assert_eq!(block_hash, current_block_hash);
+    Ok(())
+}
+
 // Check that we can subscribe to finalized blocks.
 #[tokio::test]
 async fn finalized_headers_subscription() -> Result<(), subxt::Error> {
