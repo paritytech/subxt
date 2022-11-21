15ffbb6c4b8be1dace182ff9b16c0b2f8cbd29f5
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-11-21 18:05:42 +0100

    fix(ci): wasm tests with cache + disable logging (#722)
    * fix(ci): wasm tests with cache
    
    * disable logging in wasm tests

--- testing/wasm-tests/tests/wasm.rs
+++ testing/wasm-tests/tests/wasm.rs
@@ -7,15 +7,8 @@ wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 /// Run the tests by `$ wasm-pack test --firefox --headless`
 
-fn init_tracing() {
-    console_error_panic_hook::set_once();
-    tracing_wasm::set_as_global_default();
-}
-
 #[wasm_bindgen_test]
 async fn wasm_ws_transport_works() {
-    init_tracing();
-
     let client =
         subxt::client::OnlineClient::<PolkadotConfig>::from_url("ws://127.0.0.1:9944")
             .await
