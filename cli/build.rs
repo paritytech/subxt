08d6640a6cffb12f2b756fb438165f8093887932
James Wilson <james@jsdw.me>
2023-01-17 17:29:12 +0000

    Expose version info in CLI tool with build-time obtained git hash (#787)
    * version info with built-time obtained git hash
    
    * clippy
    
    * rerun-if-changed properly and handle git command failing
    
    * cargo fmt

---
+++ cli/build.rs
@@ -0,0 +1,35 @@
+use std::{
+    borrow::Cow,
+    process::Command,
+};
+
+fn main() {
+    // Make git hash available via GIT_HASH build-time env var:
+    output_git_short_hash();
+}
+
+fn output_git_short_hash() {
+    let output = Command::new("git")
+        .args(["rev-parse", "--short=11", "HEAD"])
+        .output();
+
+    let git_hash = match output {
+        Ok(o) if o.status.success() => {
+            let sha = String::from_utf8_lossy(&o.stdout).trim().to_owned();
+            Cow::from(sha)
+        }
+        Ok(o) => {
+            println!("cargo:warning=Git command failed with status: {}", o.status);
+            Cow::from("unknown")
+        }
+        Err(err) => {
+            println!("cargo:warning=Failed to execute git command: {}", err);
+            Cow::from("unknown")
+        }
+    };
+
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    println!("cargo:rerun-if-changed=../.git/HEAD");
+    println!("cargo:rerun-if-changed=../.git/refs");
+    println!("cargo:rerun-if-changed=build.rs");
+}
