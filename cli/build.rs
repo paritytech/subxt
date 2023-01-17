e3babcb1196df4af25e75795c3ac81b03421528f
James Wilson <james@jsdw.me>
2023-01-17 11:33:37 +0000

    rerun-if-changed properly and handle git command failing

--- cli/build.rs
+++ cli/build.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::borrow::Cow;
 
 fn main() {
     // Make git hash available via GIT_HASH build-time env var:
@@ -8,13 +9,25 @@ fn main() {
 fn output_git_short_hash() {
     let output = Command::new("git")
         .args(["rev-parse", "--short=11", "HEAD"])
-        .output()
-        .expect("'git' command should exist and run successfully");
+        .output();
 
-    let git_hash =
-        String::from_utf8(output.stdout).expect("git hash should be valid UTF8");
+    let git_hash = match output {
+        Ok(o) if o.status.success() => {
+            let sha = String::from_utf8_lossy(&o.stdout).trim().to_owned();
+            Cow::from(sha)
+        },
+        Ok(o) => {
+            println!("cargo:warning=Git command failed with status: {}", o.status);
+            Cow::from("unknown")
+        },
+        Err(err) => {
+            println!("cargo:warning=Failed to execute git command: {}", err);
+            Cow::from("unknown")
+        },
+    };
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-    println!("cargo:rustc-rerun-if-changed=../.git/HEAD");
-    println!("cargo:rustc-rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=../.git/HEAD");
+    println!("cargo:rerun-if-changed=../.git/refs");
+    println!("cargo:rerun-if-changed=build.rs");
 }
