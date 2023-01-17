d4778eab6499c9d5a26c5df1f29cbcac16f82bdd
James Wilson <james@jsdw.me>
2023-01-17 12:27:24 +0000

    cargo fmt

--- cli/build.rs
+++ cli/build.rs
@@ -1,5 +1,7 @@
-use std::process::Command;
-use std::borrow::Cow;
+use std::{
+    borrow::Cow,
+    process::Command,
+};
 
 fn main() {
     // Make git hash available via GIT_HASH build-time env var:
@@ -15,15 +17,15 @@ fn output_git_short_hash() {
         Ok(o) if o.status.success() => {
             let sha = String::from_utf8_lossy(&o.stdout).trim().to_owned();
             Cow::from(sha)
-        },
+        }
         Ok(o) => {
             println!("cargo:warning=Git command failed with status: {}", o.status);
             Cow::from("unknown")
-        },
+        }
         Err(err) => {
             println!("cargo:warning=Failed to execute git command: {}", err);
             Cow::from("unknown")
-        },
+        }
     };
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
