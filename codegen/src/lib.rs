f47730fbb6d19720cf3968ff6dc6246b21ccf5e2
Alexandru Vasile <alexandru.vasile@parity.io>
2023-02-28 19:45:27 +0200

    tests: Adjust testing for the new codegen API
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -37,7 +37,9 @@
 //! let substs = TypeSubstitutes::new(&CratePath::default());
 //! // Generate the Runtime API.
 //! let generator = subxt_codegen::RuntimeGenerator::new(metadata);
-//! let runtime_api = generator.generate_runtime(item_mod, derives, substs, CratePath::default());
+//! // Include metadata documentation in the Runtime API.
+//! let generate_docs = true;
+//! let runtime_api = generator.generate_runtime(item_mod, derives, substs, CratePath::default(), generate_docs);
 //! println!("{}", runtime_api);
 //! ```
 
