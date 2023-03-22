3927b27bfece51c10066fe18cfca81bb0e26c1f1
James Wilson <james@jsdw.me>
2023-03-22 16:02:51 +0000

    actually fix the tests (hopefully..)

--- testing/integration-tests/src/frame/staking.rs
+++ testing/integration-tests/src/frame/staking.rs
@@ -68,8 +68,9 @@ async fn validate_not_possible_for_stash_account() -> Result<(), Error> {
         .wait_for_finalized_success()
         .await;
     assert_matches!(announce_validator, Err(Error::Runtime(DispatchError::Module(err))) => {
-        assert_eq!(err.details().pallet(), "Staking");
-        assert_eq!(err.details().error(), "NotController");
+        let details = err.details().unwrap();
+        assert_eq!(details.pallet(), "Staking");
+        assert_eq!(details.error(), "NotController");
     });
     Ok(())
 }
@@ -116,8 +117,9 @@ async fn nominate_not_possible_for_stash_account() -> Result<(), Error> {
         .await;
 
     assert_matches!(nomination, Err(Error::Runtime(DispatchError::Module(err))) => {
-        assert_eq!(err.details().pallet(), "Staking");
-        assert_eq!(err.details().error(), "NotController");
+        let details = err.details().unwrap();
+        assert_eq!(details.pallet(), "Staking");
+        assert_eq!(details.error(), "NotController");
     });
     Ok(())
 }
@@ -161,8 +163,9 @@ async fn chill_works_for_controller_only() -> Result<(), Error> {
         .await;
 
     assert_matches!(chill, Err(Error::Runtime(DispatchError::Module(err))) => {
-        assert_eq!(err.details().pallet(), "Staking");
-        assert_eq!(err.details().error(), "NotController");
+        let details = err.details().unwrap();
+        assert_eq!(details.pallet(), "Staking");
+        assert_eq!(details.error(), "NotController");
     });
 
     let is_chilled = api
@@ -207,8 +210,9 @@ async fn tx_bond() -> Result<(), Error> {
         .await;
 
     assert_matches!(bond_again, Err(Error::Runtime(DispatchError::Module(err))) => {
-        assert_eq!(err.details().pallet(), "Staking");
-        assert_eq!(err.details().error(), "AlreadyBonded");
+        let details = err.details().unwrap();
+        assert_eq!(details.pallet(), "Staking");
+        assert_eq!(details.error(), "AlreadyBonded");
     });
     Ok(())
 }
