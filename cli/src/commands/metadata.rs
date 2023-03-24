5ebab478ec5d5eb538b88e9fefb62a61e3fdc82e
Alexandru Vasile <alexandru.vasile@parity.io>
2023-03-24 17:53:22 +0200

    cli: Retain pallets
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- cli/src/commands/metadata.rs
+++ cli/src/commands/metadata.rs
@@ -4,11 +4,12 @@
 
 use clap::Parser as ClapParser;
 use color_eyre::eyre;
-use frame_metadata::RuntimeMetadataPrefixed;
+use frame_metadata::{RuntimeMetadata, RuntimeMetadataPrefixed};
 use jsonrpsee::client_transport::ws::Uri;
-use scale::Decode;
+use scale::{Decode, Encode};
 use std::io::{self, Write};
 use subxt_codegen::utils::fetch_metadata_hex;
+use subxt_metadata::retain_metadata_pallets;
 
 /// Download metadata from a substrate node, for use with `subxt` codegen.
 #[derive(Debug, ClapParser)]
@@ -24,25 +25,45 @@ pub struct Opts {
     /// The format of the metadata to display: `json`, `hex` or `bytes`.
     #[clap(long, short, default_value = "bytes")]
     format: String,
+    /// Generate a subset of the metadata that contains only the
+    /// types needed to represent the provided pallets.
+    #[clap(long, use_value_delimiter = true, value_parser)]
+    retain_pallets: Option<Vec<String>>,
 }
 
 pub async fn run(opts: Opts) -> color_eyre::Result<()> {
     let hex_data = fetch_metadata_hex(&opts.url).await?;
 
+    let bytes = hex::decode(hex_data.trim_start_matches("0x"))?;
+    let mut metadata = <RuntimeMetadataPrefixed as Decode>::decode(&mut &bytes[..])?;
+
+    if let Some(pallets) = opts.retain_pallets {
+        let metadata_v14 = match &mut metadata.1 {
+            RuntimeMetadata::V14(metadata_v14) => metadata_v14,
+            _ => {
+                return Err(eyre::eyre!(
+                    "Unsupported metadata version {:?}, expected V14.",
+                    metadata.1
+                ))
+            }
+        };
+
+        retain_metadata_pallets(metadata_v14, |pallet| pallets.contains(&pallet.name));
+    }
+
     match opts.format.as_str() {
         "json" => {
-            let bytes = hex::decode(hex_data.trim_start_matches("0x"))?;
-            let metadata = <RuntimeMetadataPrefixed as Decode>::decode(&mut &bytes[..])?;
             let json = serde_json::to_string_pretty(&metadata)?;
             println!("{json}");
             Ok(())
         }
         "hex" => {
+            let hex_data = format!("0x{:?}", hex::encode(metadata.encode()));
             println!("{hex_data}");
             Ok(())
         }
         "bytes" => {
-            let bytes = hex::decode(hex_data.trim_start_matches("0x"))?;
+            let bytes = metadata.encode();
             Ok(io::stdout().write_all(&bytes)?)
         }
         _ => Err(eyre::eyre!(
