ff7e53e075e108b610f6a9ac7d8c990ff1b62601
James Wilson <james@jsdw.me>
2023-04-12 16:45:20 +0100

    remove need to import parity-scale-codec crate with Subxt for macro to work

--- codegen/src/types/derives.rs
+++ codegen/src/types/derives.rs
@@ -93,12 +93,17 @@ impl Derives {
         derives.insert(syn::parse_quote!(#crate_path::ext::scale_encode::EncodeAsType));
         let encode_crate_path = quote::quote! { #crate_path::ext::scale_encode }.to_string();
         attributes.insert(syn::parse_quote!(#[encode_as_type(crate_path = #encode_crate_path)]));
+
         derives.insert(syn::parse_quote!(#crate_path::ext::scale_decode::DecodeAsType));
         let decode_crate_path = quote::quote! { #crate_path::ext::scale_decode }.to_string();
         attributes.insert(syn::parse_quote!(#[decode_as_type(crate_path = #decode_crate_path)]));
 
         derives.insert(syn::parse_quote!(#crate_path::ext::codec::Encode));
+        attributes.insert(syn::parse_quote!(#[codec(crate = #crate_path::ext::codec)]));
+
         derives.insert(syn::parse_quote!(#crate_path::ext::codec::Decode));
+        attributes.insert(syn::parse_quote!(#[codec(crate = #crate_path::ext::codec)]));
+
         derives.insert(syn::parse_quote!(Debug));
 
         Self {
