4413618844a602420a9458538e00442190bd42c6
Tadeo hepperle <tadeo@do-mix.de>
2023-04-15 12:44:05 +0200

    remove import

--- testing/integration-tests/src/blocks/mod.rs
+++ testing/integration-tests/src/blocks/mod.rs
@@ -3,7 +3,7 @@
 // see LICENSE for license details.
 
 use crate::test_context;
-use codec::{Compact, Decode};
+use codec::{Compact};
 use frame_metadata::RuntimeMetadataPrefixed;
 use futures::StreamExt;
 
