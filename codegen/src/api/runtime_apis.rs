5754c09a8a27e1d9adda4dada81fb5cd2e2d17c2
Alexandru Vasile <alexandru.vasile@parity.io>
2023-04-25 20:03:51 +0300

    codegen: Generate input structures for runtime API
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- codegen/src/api/runtime_apis.rs
+++ codegen/src/api/runtime_apis.rs
@@ -5,6 +5,7 @@
 use crate::{types::TypeGenerator, CodegenError, CratePath};
 use frame_metadata::v15::{RuntimeApiMetadata, RuntimeMetadataV15};
 use heck::ToSnakeCase as _;
+use heck::ToUpperCamelCase as _;
 
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
@@ -15,6 +16,7 @@ fn generate_runtime_api(
     metadata: &RuntimeMetadataV15,
     api: &RuntimeApiMetadata<PortableForm>,
     type_gen: &TypeGenerator,
+    types_mod_ident: &syn::Ident,
     crate_path: &CratePath,
     should_gen_docs: bool,
 ) -> Result<(TokenStream2, TokenStream2), CodegenError> {
@@ -24,11 +26,7 @@ fn generate_runtime_api(
     let trait_name_snake = format_ident!("{}", api.name.to_snake_case());
     let docs = &api.docs;
 
-    // Parameters are scale encoded into the `result` vector.
-    const ENCODED_PARAMS: &str = "result";
-    let encoded_params = format_ident!("{}", ENCODED_PARAMS);
-
-    let methods: Vec<_> = api.methods.iter().map(|method| {
+    let structs_and_methods: Vec<_> = api.methods.iter().map(|method| {
         let method_name = format_ident!("{}", method.name);
 
         // Runtime function name is `TraitName_MethodName`.
@@ -43,20 +41,24 @@ fn generate_runtime_api(
             let ty = type_gen.resolve_type_path(input.ty.id);
 
             let param = quote!(#name: #ty);
-            let encoded = quote!(#name.encode_to(&mut #encoded_params));
-            (param, encoded)
+            (param, name)
         }).collect();
 
-        // The `let [mut] result = Vec::new()` accumulator needs to be
-        // mutable only if we have inputs to add to it.
-        let encoded_mut = if inputs.is_empty() {
-            quote!()
-        } else {
-            quote!(mut)
-        };
-
         let params = inputs.iter().map(|(param, _)| param);
-        let encoded = inputs.iter().map(|(_, encoded)| encoded);
+        let param_names = inputs.iter().map(|(_, name)| name);
+
+        // From the method metadata generate a structure that holds
+        // all parameter types. This structure is used with metadata
+        // to encode parameters to the call via `encode_as_fields_to`.
+        let derives = type_gen.default_derives();
+        let struct_name = format_ident!("{}", method.name.to_upper_camel_case());
+        let struct_params = params.clone();
+        let struct_input = quote!(
+            #derives
+            pub struct #struct_name {
+                #( pub #struct_params, )*
+            }
+        );
 
         let output = type_gen.resolve_type_path(method.output.id);
 
@@ -68,36 +70,50 @@ fn generate_runtime_api(
                 ))
             };
 
-        Ok(quote!(
+        let method = quote!(
             #docs
-            pub fn #method_name(&self, #( #params, )* ) -> #crate_path::runtime_api::StaticRuntimeApiPayload<#output> {
-                let #encoded_mut #encoded_params = Vec::new();
-                #( #encoded; )*
-
-                #crate_path::runtime_api::StaticRuntimeApiPayload::new_static(
+            pub fn #method_name(&self, #( #params, )* ) -> #crate_path::runtime_api::Payload<inputs::#struct_name, #output> {
+                #crate_path::runtime_api::Payload::new_static(
                     #runtime_fn_name,
-                    #encoded_params,
+                    inputs::#struct_name { #( #param_names, )* },
                     [#(#call_hash,)*],
                 )
             }
-        ))
+        );
+
+        Ok((struct_input, method))
     }).collect::<Result<_, _>>()?;
 
     let trait_name = format_ident!("{}", trait_name);
-    // The structure that identifies the `TraitName` of the runtime call.
-    let runtime_api = quote!(
-        #( #[doc = #docs ] )*
-        pub struct #trait_name;
 
-        impl #trait_name {
-            #( #methods )*
+    let structs = structs_and_methods.iter().map(|(struct_, _)| struct_);
+    let methods = structs_and_methods.iter().map(|(_, method)| method);
+
+    let runtime_api = quote!(
+        pub mod #trait_name_snake {
+            use super::root_mod;
+            use super::#types_mod_ident;
+
+            #( #[doc = #docs ] )*
+            pub struct #trait_name;
+
+            impl #trait_name {
+                #( #methods )*
+            }
+
+            pub mod inputs {
+                use super::root_mod;
+                use super::#types_mod_ident;
+
+                #( #structs )*
+            }
         }
     );
 
     // A getter for the `RuntimeApi` to get the trait structure.
     let trait_getter = quote!(
-        pub fn #trait_name_snake(&self) -> #trait_name {
-            #trait_name
+        pub fn #trait_name_snake(&self) -> #trait_name_snake::#trait_name {
+            #trait_name_snake::#trait_name
         }
     );
 
@@ -116,7 +132,16 @@ pub fn generate_runtime_apis(
 
     let runtime_fns: Vec<_> = apis
         .iter()
-        .map(|api| generate_runtime_api(metadata, api, type_gen, crate_path, should_gen_docs))
+        .map(|api| {
+            generate_runtime_api(
+                metadata,
+                api,
+                type_gen,
+                types_mod_ident,
+                crate_path,
+                should_gen_docs,
+            )
+        })
         .collect::<Result<_, _>>()?;
 
     let runtime_apis_def = runtime_fns.iter().map(|(apis, _)| apis);
