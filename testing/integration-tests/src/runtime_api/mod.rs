3866c6b89839b8b758582bc8d1b05b45ec8b23e3
Alexandru Vasile <alexandru.vasile@parity.io>
2023-04-27 18:07:41 +0300

    testing: Check Alice account nonce before submitting the tx
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- testing/integration-tests/src/runtime_api/mod.rs
+++ testing/integration-tests/src/runtime_api/mod.rs
@@ -14,6 +14,18 @@ async fn account_nonce() -> Result<(), subxt::Error> {
     let signer = pair_signer(AccountKeyring::Alice.pair());
     let alice: AccountId32 = AccountKeyring::Alice.to_account_id().into();
 
+    // Check Alice nonce is starting from 0.
+    let runtime_api_call = node_runtime::apis()
+        .account_nonce_api()
+        .account_nonce(alice);
+    let nonce = api
+        .runtime_api()
+        .at_latest()
+        .await?
+        .call(runtime_api_call)
+        .await?;
+    assert_eq!(nonce, 0);
+
     // Do some transaction to bump the Alice nonce to 1:
     let remark_tx = node_runtime::tx().system().remark(vec![1, 2, 3, 4, 5]);
     api.tx()
