cf67944cc9a29e8e26bb736e704185375d165af0
James Wilson <james@jsdw.me>
2023-06-19 14:46:28 +0100

    Add missing license header

--- signer/src/crypto/derive_junction.rs
+++ signer/src/crypto/derive_junction.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Parity Technologies (UK) Ltd.
+// This file is dual-licensed as Apache-2.0 or GPL-3.0.
+// see LICENSE for license details.
+
 use codec::{Decode, Encode};
 
 /// The length of the junction identifier. Note that this is also referred to as the
