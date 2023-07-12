66b7d2ea80caa6faba927994d213cf2502b33149
James Wilson <james@jsdw.me>
2023-07-12 17:53:26 +0100

    fix wasm test

--- signer/wasm-tests/tests/wasm.rs
+++ signer/wasm-tests/tests/wasm.rs
@@ -1,6 +1,6 @@
 #![cfg(target_arch = "wasm32")]
 
-use subxt_signer::sr25519;
+use subxt_signer::{ ecdsa, sr25519 };
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
