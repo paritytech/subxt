6ecd7559a683f7c882209d246836f4604f0ee636
Tadeo Hepperle <62739623+tadeohepperle@users.noreply.github.com>
2023-08-09 19:04:02 +0200

    Support more types in Storage entry constructors  (#1105)
    * implement test for type alias being used
    
    * Bump serde from 1.0.179 to 1.0.183 (#1112)
    
    Bumps [serde](https://github.com/serde-rs/serde) from 1.0.179 to 1.0.183.
    - [Release notes](https://github.com/serde-rs/serde/releases)
    - [Commits](https://github.com/serde-rs/serde/compare/v1.0.179...v1.0.183)
    
    ---
    updated-dependencies:
    - dependency-name: serde
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump regex from 1.9.1 to 1.9.3 (#1110)
    
    Bumps [regex](https://github.com/rust-lang/regex) from 1.9.1 to 1.9.3.
    - [Release notes](https://github.com/rust-lang/regex/releases)
    - [Changelog](https://github.com/rust-lang/regex/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rust-lang/regex/compare/1.9.1...1.9.3)
    
    ---
    updated-dependencies:
    - dependency-name: regex
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * revert yaml changes
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- codegen/src/api/storage.rs
+++ codegen/src/api/storage.rs
@@ -5,7 +5,7 @@
 use crate::types::TypePath;
 use crate::{types::TypeGenerator, CratePath};
 use heck::ToSnakeCase as _;
-use proc_macro2::{Ident, TokenStream as TokenStream2};
+use proc_macro2::{Ident, TokenStream as TokenStream2, TokenStream};
 use quote::{format_ident, quote};
 use scale_info::TypeDef;
 use subxt_metadata::{
@@ -125,14 +125,7 @@ fn generate_storage_entry_fns(
         let is_iterable_type = is_iterable.then_some(quote!(#crate_path::storage::address::Yes)).unwrap_or(quote!(()));
         let key_impls = keys_slice.iter().map(|(field_name, _)| quote!( #crate_path::storage::address::make_static_storage_map_key(#field_name.borrow()) ));
         let key_args = keys_slice.iter().map(|(field_name, field_type)| {
-            // The field type is translated from `std::vec::Vec<T>` to `[T]`. We apply
-            // Borrow to all types, so this just makes it a little more ergonomic.
-            // TODO [jsdw]: Support mappings like `String -> str` too for better borrow
-            // ergonomics.
-            let field_ty = match field_type.vec_type_param() {
-                Some(ty) => quote!([#ty]),
-                _ => quote!(#field_type),
-            };
+            let field_ty = primitive_type_alias(field_type);
             quote!( #field_name: impl ::std::borrow::Borrow<#field_ty> )
         });
 
@@ -163,4 +156,137 @@ fn generate_storage_entry_fns(
 
         )*
     })
+}
+
+fn primitive_type_alias(type_path: &TypePath) -> TokenStream {
+    // Vec<T> is cast to [T]
+    if let Some(ty) = type_path.vec_type_param() {
+        return quote!([#ty]);
+    }
+    // String is cast to str
+    if type_path.is_string() {
+        return quote!(::core::primitive::str);
+    }
+    quote!(#type_path)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::RuntimeGenerator;
+    use frame_metadata::v15;
+    use quote::{format_ident, quote};
+    use scale_info::{meta_type, MetaType};
+    use std::borrow::Cow;
+
+    use subxt_metadata::Metadata;
+
+    fn metadata_with_storage_entries(
+        storage_entries: impl IntoIterator<Item = (&'static str, MetaType)>,
+    ) -> Metadata {
+        let storage_entries: Vec<v15::StorageEntryMetadata> = storage_entries
+            .into_iter()
+            .map(|(name, key)| v15::StorageEntryMetadata {
+                name,
+                modifier: v15::StorageEntryModifier::Optional,
+                ty: v15::StorageEntryType::Map {
+                    hashers: vec![],
+                    key,
+                    value: meta_type::<bool>(),
+                },
+                default: vec![],
+                docs: vec![],
+            })
+            .collect();
+
+        let pallet_1 = v15::PalletMetadata {
+            name: "Pallet1",
+            storage: Some(v15::PalletStorageMetadata {
+                prefix: Default::default(),
+                entries: storage_entries,
+            }),
+            calls: None,
+            event: None,
+            constants: vec![],
+            error: None,
+            index: 0,
+            docs: vec![],
+        };
+
+        let extrinsic_metadata = v15::ExtrinsicMetadata {
+            version: 0,
+            signed_extensions: vec![],
+            address_ty: meta_type::<()>(),
+            call_ty: meta_type::<()>(),
+            signature_ty: meta_type::<()>(),
+            extra_ty: meta_type::<()>(),
+        };
+
+        let metadata: Metadata = v15::RuntimeMetadataV15::new(
+            vec![pallet_1],
+            extrinsic_metadata,
+            meta_type::<()>(),
+            vec![],
+            v15::OuterEnums {
+                call_enum_ty: meta_type::<()>(),
+                event_enum_ty: meta_type::<()>(),
+                error_enum_ty: meta_type::<()>(),
+            },
+            v15::CustomMetadata {
+                map: Default::default(),
+            },
+        )
+        .try_into()
+        .expect("can build valid metadata");
+        metadata
+    }
+
+    #[test]
+    fn borrow_type_replacements() {
+        let storage_entries = [
+            ("vector", meta_type::<Vec<u8>>()),
+            ("boxed", meta_type::<Box<u16>>()),
+            ("string", meta_type::<String>()),
+            ("static_string", meta_type::<&'static str>()),
+            ("cow_string", meta_type::<Cow<'_, str>>()),
+        ];
+
+        let expected_borrowed_types = [
+            quote!([::core::primitive::u8]),
+            quote!(::core::primitive::u16),
+            quote!(::core::primitive::str),
+            quote!(::core::primitive::str),
+            quote!(::core::primitive::str),
+        ];
+
+        let metadata = metadata_with_storage_entries(storage_entries);
+
+        let item_mod = syn::parse_quote!(
+            pub mod api {}
+        );
+        let generator = RuntimeGenerator::new(metadata);
+        let generated = generator
+            .generate_runtime(
+                item_mod,
+                Default::default(),
+                Default::default(),
+                "::subxt_path".into(),
+                false,
+            )
+            .expect("should be able to generate runtime");
+        let generated_str = generated.to_string();
+
+        for ((name, _), expected_type) in storage_entries
+            .into_iter()
+            .zip(expected_borrowed_types.into_iter())
+        {
+            let name_ident = format_ident!("{}", name);
+            let expected_storage_constructor = quote!(
+                fn #name_ident(
+                    &self,
+                    _0: impl ::std::borrow::Borrow<#expected_type>,
+                )
+            );
+            assert!(generated_str.contains(&expected_storage_constructor.to_string()));
+        }
+    }
 }
