cd52f847c4fda3855b7ea44906433bb4cfdad7df
James Wilson <james@jsdw.me>
2023-10-09 13:06:12 +0200

    Wee tidy to subxt-signer flags

--- signer/src/lib.rs
+++ signer/src/lib.rs
@@ -39,5 +39,5 @@ pub use secrecy::{ExposeSecret, SecretString};
 // DeriveJunctions are the "path" part of these SecretUris.
 pub use crypto::{DeriveJunction, SecretUri, SecretUriError, DEV_PHRASE};
 
-#[cfg(all(feature = "subxt", not(any(feature = "web", feature = "native"))))]
-compile_error!("subxt-signer: When using the 'subxt' feature, exactly one of the 'web' and 'native' features should be used.");
+#[cfg(not(any(feature = "web", feature = "native")))]
+compile_error!("subxt-signer: exactly one of the 'web' and 'native' features should be used.");
