0eabab961805ef983479a4dc2bf54e35a8c922d4
Tadeo hepperle <tadeo@do-mix.de>
2023-10-24 13:18:20 +0200

    clippy

--- testing/integration-tests/src/full_client/blocks/mod.rs
+++ testing/integration-tests/src/full_client/blocks/mod.rs
@@ -2,12 +2,12 @@
 // This file is dual-licensed as Apache-2.0 or GPL-3.0.
 // see LICENSE for license details.
 
-use crate::utils::TestContext;
+
 use crate::{test_context, utils::node_runtime};
 use codec::{Compact, Encode};
 use futures::StreamExt;
-use std::future::Future;
-use std::time::Duration;
+
+
 use subxt::config::DefaultExtrinsicParamsBuilder;
 use subxt_metadata::Metadata;
 use subxt_signer::sr25519::dev;
