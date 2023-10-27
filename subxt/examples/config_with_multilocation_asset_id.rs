7f77a098859f8c917b17f87a17f7aa8f23a112c5
Tadeo hepperle <tadeo@do-mix.de>
2023-10-27 11:23:12 +0200

    fmt

--- subxt/examples/config_with_multilocation_asset_id.rs
+++ subxt/examples/config_with_multilocation_asset_id.rs
@@ -1,10 +1,9 @@
-
 //! This example just shows how to use MultiLocation as the type of the AssetId in the subxt Config. Some chains may use something like this.
 
 #[subxt::subxt(runtime_metadata_path = "../artifacts/polkadot_metadata_full.scale")]
 pub mod polkadot {}
 
-use subxt::{Config, config::DefaultExtrinsicParams, SubstrateConfig};
+use subxt::{config::DefaultExtrinsicParams, Config, SubstrateConfig};
 
 enum MyConfig {}
 
@@ -12,15 +11,11 @@ impl Config for MyConfig {
     type Hash = <SubstrateConfig as Config>::Hash;
     type AccountId = <SubstrateConfig as Config>::AccountId;
     type Address = <SubstrateConfig as Config>::Address;
-    type Signature =<SubstrateConfig as Config>::Signature;
+    type Signature = <SubstrateConfig as Config>::Signature;
     type Hasher = <SubstrateConfig as Config>::Hasher;
     type Header = <SubstrateConfig as Config>::Header;
     type ExtrinsicParams = DefaultExtrinsicParams<Self>;
     type AssetId = polkadot::runtime_types::xcm::v2::multilocation::MultiLocation;
 }
 
-
-
-pub fn main(){
-    
-}
+pub fn main() {}
