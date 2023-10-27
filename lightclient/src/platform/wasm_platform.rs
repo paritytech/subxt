409c54d1ad02d1b5ae37c56ecd880881997a1d3c
Alexandru Vasile <alexandru.vasile@parity.io>
2023-10-27 15:48:18 +0300

    lightclient: Update `SubxtPlatform`
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- lightclient/src/platform/wasm_platform.rs
+++ lightclient/src/platform/wasm_platform.rs
@@ -2,21 +2,22 @@
 // This file is dual-licensed as Apache-2.0 or GPL-3.0.
 // see LICENSE for license details.
 
+use super::wasm_socket::WasmSocket;
+
 use core::time::Duration;
-use futures::{prelude::*, task::Poll};
-
-use smoldot::libp2p::multiaddr::Multiaddr;
+use futures::prelude::*;
 use smoldot_light::platform::{
-    ConnectError, PlatformConnection, PlatformRef, PlatformSubstreamDirection, ReadBuffer,
+    Address, ConnectError, ConnectionType, IpAddr, MultiStreamAddress, MultiStreamWebRtcConnection,
+    PlatformRef, SubstreamDirection,
 };
-use std::{io::IoSlice, pin::Pin};
+use std::{io, net::SocketAddr, pin::Pin};
 
-use super::wasm_helpers::{StreamReadBuffer, StreamWriteBuffer};
+const LOG_TARGET: &str = "subxt-platform-wasm";
 
-/// Subxt plaform implementation for wasm.
+/// Subxt platform implementation for wasm.
 ///
 /// This implementation is a conversion of the implementation from the smoldot:
-/// https://github.com/smol-dot/smoldot/blob/f49ce4ea6a325c444ab6ad37d3ab5558edf0d541/light-base/src/platform/default.rs#L52.
+/// https://github.com/smol-dot/smoldot/blob/6401d4df90569e23073d646b14a8fbf9f7e6bdd3/light-base/src/platform/default.rs#L83.
 ///
 /// This platform will evolve over time and we'll need to keep this code in sync.
 #[derive(Clone)]
@@ -30,17 +31,18 @@ impl SubxtPlatform {
 
 impl PlatformRef for SubxtPlatform {
     type Delay = super::wasm_helpers::Delay;
-    type Yield = future::Ready<()>;
     type Instant = super::wasm_helpers::Instant;
-    type Connection = std::convert::Infallible;
+    type MultiStream = std::convert::Infallible;
     type Stream = super::wasm_helpers::Stream;
-    type ConnectFuture = future::BoxFuture<
+    type StreamConnectFuture = future::BoxFuture<'static, Result<Self::Stream, ConnectError>>;
+    type MultiStreamConnectFuture = future::BoxFuture<
         'static,
-        Result<PlatformConnection<Self::Stream, Self::Connection>, ConnectError>,
+        Result<MultiStreamWebRtcConnection<Self::MultiStream>, ConnectError>,
     >;
+    type ReadWriteAccess<'a> = super::wasm_helpers::with_buffers::ReadWriteAccess<'a>;
     type StreamUpdateFuture<'a> = future::BoxFuture<'a, ()>;
-    type NextSubstreamFuture<'a> =
-        future::Pending<Option<(Self::Stream, PlatformSubstreamDirection)>>;
+    type StreamErrorRef<'a> = &'a std::io::Error;
+    type NextSubstreamFuture<'a> = future::Pending<Option<(Self::Stream, SubstreamDirection)>>;
 
     fn now_from_unix_epoch(&self) -> Duration {
         super::wasm_helpers::now_from_unix_epoch()
@@ -48,6 +50,10 @@ impl PlatformRef for SubxtPlatform {
 
     fn now(&self) -> Self::Instant {
         super::wasm_helpers::now()
+    }
+
+    fn fill_random_bytes(&self, buffer: &mut [u8]) {
+        rand::RngCore::fill_bytes(&mut rand::thread_rng(), buffer);
     }
 
     fn sleep(&self, duration: Duration) -> Self::Delay {
@@ -58,251 +64,12 @@ impl PlatformRef for SubxtPlatform {
         self.sleep(when.saturating_duration_since(self.now()))
     }
 
-    fn yield_after_cpu_intensive(&self) -> Self::Yield {
-        // No-op.
-        future::ready(())
-    }
-
-    fn connect(&self, multiaddr: &str) -> Self::ConnectFuture {
-        // We simply copy the address to own it. We could be more zero-cost here, but doing so
-        // would considerably complicate the implementation.
-        let multiaddr = multiaddr.to_owned();
-
-        tracing::debug!("Connecting to multiaddress={:?}", multiaddr);
-
-        Box::pin(async move {
-            let addr = multiaddr.parse::<Multiaddr>().map_err(|_| ConnectError {
-                is_bad_addr: true,
-                message: "Failed to parse address".to_string(),
-            })?;
-
-            let mut iter = addr.iter().fuse();
-            let proto1 = iter.next().ok_or(ConnectError {
-                is_bad_addr: true,
-                message: "Unknown protocols combination".to_string(),
-            })?;
-            let proto2 = iter.next().ok_or(ConnectError {
-                is_bad_addr: true,
-                message: "Unknown protocols combination".to_string(),
-            })?;
-            let proto3 = iter.next();
-
-            if iter.next().is_some() {
-                return Err(ConnectError {
-                    is_bad_addr: true,
-                    message: "Unknown protocols combination".to_string(),
-                });
-            }
-
-            super::wasm_helpers::connect(proto1, proto2, proto3).await
-        })
-    }
-
-    fn open_out_substream(&self, c: &mut Self::Connection) {
-        // This function can only be called with so-called "multi-stream" connections. We never
-        // open such connection.
-        match *c {}
-    }
-
-    fn next_substream<'a>(&self, c: &'a mut Self::Connection) -> Self::NextSubstreamFuture<'a> {
-        // This function can only be called with so-called "multi-stream" connections. We never
-        // open such connection.
-        match *c {}
-    }
-
-    fn update_stream<'a>(&self, stream: &'a mut Self::Stream) -> Self::StreamUpdateFuture<'a> {
-        Box::pin(future::poll_fn(|cx| {
-            // The `connect` is expected to be called before this method and would populate
-            // the buffers properly. When the buffers are empty, this future is shortly dropped.
-            let Some((read_buffer, write_buffer)) = stream.buffers.as_mut() else {
-                return Poll::Pending;
-            };
-
-            // Whether the future returned by `update_stream` should return `Ready` or `Pending`.
-            let mut update_stream_future_ready = false;
-
-            if let StreamReadBuffer::Open {
-                buffer: ref mut buf,
-                ref mut cursor,
-            } = read_buffer
-            {
-                // When reading data from the socket, `poll_read` might return "EOF". In that
-                // situation, we transition to the `Closed` state, which would discard the data
-                // currently in the buffer. For this reason, we only try to read if there is no
-                // data left in the buffer.
-                if cursor.start == cursor.end {
-                    if let Poll::Ready(result) = Pin::new(&mut stream.socket).poll_read(cx, buf) {
-                        update_stream_future_ready = true;
-                        match result {
-                            Err(_) => {
-                                // End the stream.
-                                stream.buffers = None;
-                                return Poll::Ready(());
-                            }
-                            Ok(0) => {
-                                // EOF.
-                                *read_buffer = StreamReadBuffer::Closed;
-                            }
-                            Ok(bytes) => {
-                                *cursor = 0..bytes;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if let StreamWriteBuffer::Open {
-                buffer: ref mut buf,
-                must_flush,
-                must_close,
-            } = write_buffer
-            {
-                while !buf.is_empty() {
-                    let write_queue_slices = buf.as_slices();
-                    if let Poll::Ready(result) = Pin::new(&mut stream.socket).poll_write_vectored(
-                        cx,
-                        &[
-                            IoSlice::new(write_queue_slices.0),
-                            IoSlice::new(write_queue_slices.1),
-                        ],
-                    ) {
-                        if !*must_close {
-                            // In the situation where the API user wants to close the writing
-                            // side, simply sending the buffered data isn't enough to justify
-                            // making the future ready.
-                            update_stream_future_ready = true;
-                        }
-
-                        match result {
-                            Err(_) => {
-                                // End the stream.
-                                stream.buffers = None;
-                                return Poll::Ready(());
-                            }
-                            Ok(bytes) => {
-                                *must_flush = true;
-                                for _ in 0..bytes {
-                                    buf.pop_front();
-                                }
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-                }
-
-                if buf.is_empty() && *must_close {
-                    if let Poll::Ready(result) = Pin::new(&mut stream.socket).poll_close(cx) {
-                        update_stream_future_ready = true;
-                        match result {
-                            Err(_) => {
-                                // End the stream.
-                                stream.buffers = None;
-                                return Poll::Ready(());
-                            }
-                            Ok(()) => {
-                                *write_buffer = StreamWriteBuffer::Closed;
-                            }
-                        }
-                    }
-                } else if *must_flush {
-                    if let Poll::Ready(result) = Pin::new(&mut stream.socket).poll_flush(cx) {
-                        update_stream_future_ready = true;
-                        match result {
-                            Err(_) => {
-                                // End the stream.
-                                stream.buffers = None;
-                                return Poll::Ready(());
-                            }
-                            Ok(()) => {
-                                *must_flush = false;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if update_stream_future_ready {
-                Poll::Ready(())
-            } else {
-                // Progress cannot be made since poll_read, poll_write, poll_close, poll_flush
-                // are not ready yet. Smoldot drops this future and calls it again with the
-                // next processing iteration.
-                Poll::Pending
-            }
-        }))
-    }
-
-    fn read_buffer<'a>(&self, stream: &'a mut Self::Stream) -> ReadBuffer<'a> {
-        match stream.buffers.as_ref().map(|(r, _)| r) {
-            None => ReadBuffer::Reset,
-            Some(StreamReadBuffer::Closed) => ReadBuffer::Closed,
-            Some(StreamReadBuffer::Open { buffer, cursor }) => {
-                ReadBuffer::Open(&buffer[cursor.clone()])
-            }
-        }
-    }
-
-    fn advance_read_cursor(&self, stream: &mut Self::Stream, extra_bytes: usize) {
-        let Some(StreamReadBuffer::Open { ref mut cursor, .. }) =
-            stream.buffers.as_mut().map(|(r, _)| r)
-        else {
-            assert_eq!(extra_bytes, 0);
-            return;
-        };
-
-        assert!(cursor.start + extra_bytes <= cursor.end);
-        cursor.start += extra_bytes;
-    }
-
-    fn writable_bytes(&self, stream: &mut Self::Stream) -> usize {
-        let Some(StreamWriteBuffer::Open {
-            ref mut buffer,
-            must_close: false,
-            ..
-        }) = stream.buffers.as_mut().map(|(_, w)| w)
-        else {
-            return 0;
-        };
-        buffer.capacity() - buffer.len()
-    }
-
-    fn send(&self, stream: &mut Self::Stream, data: &[u8]) {
-        debug_assert!(!data.is_empty());
-
-        // Because `writable_bytes` returns 0 if the writing side is closed, and because `data`
-        // must always have a size inferior or equal to `writable_bytes`, we know for sure that
-        // the writing side isn't closed.
-        let Some(StreamWriteBuffer::Open { ref mut buffer, .. }) =
-            stream.buffers.as_mut().map(|(_, w)| w)
-        else {
-            panic!()
-        };
-        buffer.reserve(data.len());
-        buffer.extend(data.iter().copied());
-    }
-
-    fn close_send(&self, stream: &mut Self::Stream) {
-        // It is not illegal to call this on an already-reset stream.
-        let Some((_, write_buffer)) = stream.buffers.as_mut() else {
-            return;
-        };
-
-        match write_buffer {
-            StreamWriteBuffer::Open {
-                must_close: must_close @ false,
-                ..
-            } => *must_close = true,
-            _ => {
-                // However, it is illegal to call this on a stream that was already close
-                // attempted.
-                panic!()
-            }
-        }
-    }
-
-    fn spawn_task(&self, _: std::borrow::Cow<str>, task: future::BoxFuture<'static, ()>) {
-        super::wasm_helpers::spawn(task);
+    fn spawn_task(
+        &self,
+        _task_name: std::borrow::Cow<str>,
+        task: impl future::Future<Output = ()> + Send + 'static,
+    ) {
+        wasm_bindgen_futures::spawn_local(task);
     }
 
     fn client_name(&self) -> std::borrow::Cow<str> {
@@ -311,5 +78,113 @@ impl PlatformRef for SubxtPlatform {
 
     fn client_version(&self) -> std::borrow::Cow<str> {
         env!("CARGO_PKG_VERSION").into()
+    }
+
+    fn supports_connection_type(&self, connection_type: ConnectionType) -> bool {
+        let result = matches!(
+            connection_type,
+            ConnectionType::WebSocketIpv4 { .. }
+                | ConnectionType::WebSocketIpv6 { .. }
+                | ConnectionType::WebSocketDns { .. }
+        );
+
+        tracing::trace!(
+            target: LOG_TARGET,
+            "Supports connection type={:?} result={}",
+            connection_type, result
+        );
+
+        result
+    }
+
+    fn connect_stream(&self, multiaddr: Address) -> Self::StreamConnectFuture {
+        tracing::trace!(target: LOG_TARGET, "Connect stream to multiaddr={:?}", multiaddr);
+
+        // `PlatformRef` trait guarantees that `connect_stream` is only called with addresses
+        // stated in `supports_connection_type`.
+        let addr = match multiaddr {
+            Address::WebSocketDns {
+                hostname,
+                port,
+                secure: true,
+            } => {
+                format!("wss://{}:{}", hostname, port)
+            }
+            Address::WebSocketDns {
+                hostname,
+                port,
+                secure: false,
+            } => {
+                format!("ws://{}:{}", hostname, port)
+            }
+            Address::WebSocketIp {
+                ip: IpAddr::V4(ip),
+                port,
+            } => {
+                let addr = SocketAddr::from((ip, port));
+                format!("ws://{}", addr.to_string())
+            }
+            Address::WebSocketIp {
+                ip: IpAddr::V6(ip),
+                port,
+            } => {
+                let addr = SocketAddr::from((ip, port));
+                format!("ws://{}", addr.to_string())
+            }
+
+            // The API user of the `PlatformRef` trait is never supposed to open connections of
+            // a type that isn't supported.
+            _ => {
+                unreachable!("Connecting to an address not supported. This code path indicates a bug in smoldot. Please raise an issue at https://github.com/smol-dot/smoldot/issues")
+            }
+        };
+
+        Box::pin(async move {
+            tracing::debug!(target: LOG_TARGET, "Connecting to addr={addr}");
+
+            let socket = WasmSocket::new(addr.as_str()).map_err(|err| ConnectError {
+                message: format!("Failed to reach peer: {err}"),
+            })?;
+
+            Ok(super::wasm_helpers::Stream(
+                super::wasm_helpers::with_buffers::WithBuffers::new(socket),
+            ))
+        })
+    }
+
+    fn connect_multistream(&self, _address: MultiStreamAddress) -> Self::MultiStreamConnectFuture {
+        panic!("Multistreams are not currently supported. This code path indicates a bug in smoldot. Please raise an issue at https://github.com/smol-dot/smoldot/issues")
+    }
+
+    fn open_out_substream(&self, c: &mut Self::MultiStream) {
+        // This function can only be called with so-called "multi-stream" connections. We never
+        // open such connection.
+        match *c {}
+    }
+
+    fn next_substream(&self, c: &'_ mut Self::MultiStream) -> Self::NextSubstreamFuture<'_> {
+        // This function can only be called with so-called "multi-stream" connections. We never
+        // open such connection.
+        match *c {}
+    }
+
+    fn read_write_access<'a>(
+        &self,
+        stream: Pin<&'a mut Self::Stream>,
+    ) -> Result<Self::ReadWriteAccess<'a>, &'a io::Error> {
+        let stream = stream.project();
+        stream.0.read_write_access(Self::Instant::now())
+    }
+
+    fn wait_read_write_again<'a>(
+        &self,
+        stream: Pin<&'a mut Self::Stream>,
+    ) -> Self::StreamUpdateFuture<'a> {
+        let stream = stream.project();
+        Box::pin(stream.0.wait_read_write_again(|when| async move {
+            let now = super::wasm_helpers::now();
+            let duration = when.saturating_duration_since(now);
+            super::wasm_helpers::sleep(duration).await;
+        }))
     }
 }
