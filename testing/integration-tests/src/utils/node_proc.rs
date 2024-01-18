240cf267a95ee23952221fc86179436c0fc6d477
James Wilson <james@jsdw.me>
2024-01-18 10:27:16 +0000

    resolve clippy err

--- testing/integration-tests/src/utils/node_proc.rs
+++ testing/integration-tests/src/utils/node_proc.rs
@@ -223,7 +223,6 @@ async fn build_unstable_client<T: Config>(
                 // ensure that the message makes it to some logs.
                 eprintln!("Error driving unstable backend in tests (will panic): {e}");
                 panic!("Error driving unstable backend in tests: {e}");
-                break;
             }
         }
     });
