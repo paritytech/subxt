f29fbed2ba998b6dbd5ab4f19f25dd1acb672e19
Tadeo Hepperle <62739623+tadeohepperle@users.noreply.github.com>
2024-01-18 16:39:17 +0100

    Improve Signed Extension and Block Decoding Examples/Book (#1357)
    * asset hub example and book adjustment
    
    * formatting
    
    * recursive derives
    
    * polkadot monitor example and book adjustments
    
    * formatting
    
    * adjust docs and examples, add dynamic example
    
    * james suggestions
    
    * fmt
    
    * chore(subxt/src): typo fix (#1370)
    
    * rpcmethods
    
    * followstr
    
    * mod and else
    
    * Weekly Cronjob fetching artifacts and generating polkadot.rs file. (#1352)
    
    * github CI action cronjob
    
    * add commit message
    
    * fix the CI yml files
    
    * binary crate for CI script with substrate-runner
    
    * update the CI script
    
    * correct the artifacts script
    
    * remove bash script
    
    ---------
    
    Co-authored-by: James Wilson <james@jsdw.me>
    Co-authored-by: Pan chao <152830401+Pan-chao@users.noreply.github.com>

---
+++ subxt/examples/block_decoding_static.rs
@@ -0,0 +1,65 @@
+#![allow(missing_docs)]
+use subxt::{
+    utils::{AccountId32, MultiAddress},
+    OnlineClient, PolkadotConfig,
+};
+
+use codec::Decode;
+
+#[subxt::subxt(runtime_metadata_path = "../artifacts/polkadot_metadata_small.scale")]
+pub mod polkadot {}
+
+use polkadot::balances::calls::types::TransferKeepAlive;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a client that subscribes to blocks of the Polkadot network.
+    let api = OnlineClient::<PolkadotConfig>::from_url("wss://rpc.polkadot.io:443").await?;
+
+    // Subscribe to all finalized blocks:
+    let mut blocks_sub = api.blocks().subscribe_finalized().await?;
+
+    // For each block, print details about the `TransferKeepAlive` transactions we are interested in.
+    while let Some(block) = blocks_sub.next().await {
+        let block = block?;
+        let block_number = block.header().number;
+        let block_hash = block.hash();
+        println!("Block #{block_number}  ({block_hash}):");
+
+        let extrinsics = block.extrinsics().await?;
+        for ext in extrinsics.iter() {
+            let ext = ext?;
+            if let Ok(Some(transfer)) = ext.as_extrinsic::<TransferKeepAlive>() {
+                let Some(extensions) = ext.signed_extensions() else {
+                    panic!("TransferKeepAlive should be signed")
+                };
+
+                ext.address_bytes().unwrap();
+                let addr_bytes = ext
+                    .address_bytes()
+                    .expect("TransferKeepAlive should be signed");
+                let sender = MultiAddress::<AccountId32, ()>::decode(&mut &addr_bytes[..])
+                    .expect("Decoding should work");
+                let sender = display_address(&sender);
+                let receiver = display_address(&transfer.dest);
+                let value = transfer.value;
+                let tip = extensions.tip().expect("Should have tip");
+                let nonce = extensions.nonce().expect("Should have nonce");
+
+                println!(
+                    "    Transfer of {value} DOT:\n        {sender} (Tip: {tip}, Nonce: {nonce}) ---> {receiver}",
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn display_address(addr: &MultiAddress<AccountId32, ()>) -> String {
+    if let MultiAddress::Id(id32) = addr {
+        format!("{id32}")
+    } else {
+        "MultiAddress::...".into()
+    }
+}
