890c051926044643748416b0aaa43851954ea4bc
Alexandru Vasile <alexandru.vasile@parity.io>
2024-02-12 13:23:56 +0200

    subxt/storage: Derive std::cmp traits
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- subxt/src/storage/storage_address.rs
+++ subxt/src/storage/storage_address.rs
@@ -55,7 +55,11 @@ pub struct Yes;
 #[derive(Derivative)]
 #[derivative(
     Clone(bound = "StorageKey: Clone"),
-    Debug(bound = "StorageKey: std::fmt::Debug")
+    Debug(bound = "StorageKey: std::fmt::Debug"),
+    Eq(bound = "StorageKey: std::cmp::Eq"),
+    Ord(bound = "StorageKey: std::cmp::Ord"),
+    PartialEq(bound = "StorageKey: std::cmp::PartialEq"),
+    PartialOrd(bound = "StorageKey: std::cmp::PartialOrd")
 )]
 pub struct Address<StorageKey, ReturnTy, Fetchable, Defaultable, Iterable> {
     pallet_name: Cow<'static, str>,
