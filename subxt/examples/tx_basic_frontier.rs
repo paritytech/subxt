7ee0c223654ddad52f53878fe9bdaa218bb54eec
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-04-25 14:19:41 +0200

    address grumbles

--- subxt/examples/tx_basic_frontier.rs
+++ subxt/examples/tx_basic_frontier.rs
@@ -1,9 +1,14 @@
+//! Example to use subxt to talk to substrate-based nodes with ethereum accounts
+//! which is not the default for subxt which is why we need to provide a custom config.
+//!
+//! This example requires to run a local frontier/moonbeam node to work.
+
 #![allow(missing_docs)]
 
 use subxt::OnlineClient;
 use subxt_signer::eth::{dev, AccountId20, Signature};
 
-#[subxt::subxt(runtime_metadata_insecure_url = "ws://127.0.0.1:9944")]
+#[subxt::subxt(runtime_metadata_path = "../artifacts/frontier_metadata_small.scale")]
 mod eth_runtime {}
 
 enum EthRuntimeConfig {}
@@ -20,12 +25,12 @@ impl subxt::Config for EthRuntimeConfig {
     type AssetId = u32;
 }
 
-// This helper makes it easy to use our `eth_signer::AccountId20`'s with generated
-// code that expects a generated `eth_runtime::runtime_types::foo::AccountId20` type.
-// an alternative is to do some type substitution in the generated code itself, but
-// mostly I'd avoid doing that unless absolutely necessary.
-fn to_runtime_account_id(a: AccountId20) -> eth_runtime::runtime_types::fp_account::AccountId20 {
-    eth_runtime::runtime_types::fp_account::AccountId20(a.0)
+// This helper makes it easy to use our `AccountId20`'s with generated
+// code that expects a generated `eth_runtime::runtime_types::fp_account:AccountId20` type.
+impl From<AccountId20> for eth_runtime::runtime_types::fp_account::AccountId20 {
+    fn from(a: AccountId20) -> Self {
+        eth_runtime::runtime_types::fp_account::AccountId20(a.0)
+    }
 }
 
 #[tokio::main]
@@ -41,7 +46,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let balance_transfer_tx = eth_runtime::tx()
         .balances()
-        .transfer_allow_death(to_runtime_account_id(dest), 10_001);
+        .transfer_allow_death(dest.into(), 10_001);
 
     let events = api
         .tx()
