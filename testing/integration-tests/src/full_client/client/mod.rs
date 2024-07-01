25583dcc654008f2e0de8a1dfc8b24e5fa36c518
Alexandru Vasile <60601340+lexnv@users.noreply.github.com>
2024-07-01 14:19:22 +0300

    testing: Modify the storage_fetch_raw_keys to reflect substrate-binary (#1667)
    * testing: Modify the storage_fetch_raw_keys to reflect substrate-binary
    changes
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * 13 -> 14 accounts
    
    ---------
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

--- testing/integration-tests/src/full_client/client/mod.rs
+++ testing/integration-tests/src/full_client/client/mod.rs
@@ -42,7 +42,7 @@ async fn storage_fetch_raw_keys() {
         .count()
         .await;
 
-    assert_eq!(len, 13)
+    assert_eq!(len, 14)
 }
 
 #[cfg(fullclient)]
@@ -67,7 +67,7 @@ async fn storage_iter() {
         .count()
         .await;
 
-    assert_eq!(len, 13);
+    assert_eq!(len, 14);
 }
 
 #[cfg(fullclient)]
