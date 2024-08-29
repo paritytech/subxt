4ee2cdc8194a5bcc8704b0bfd9bd10bda3ac4569
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-08-29 17:39:03 +0200

    Update testing/substrate-runner/src/lib.rs

--- testing/substrate-runner/src/lib.rs
+++ testing/substrate-runner/src/lib.rs
@@ -198,7 +198,7 @@ fn try_find_substrate_port_from_output(
             .map(|(_, port_str)| port_str);
 
         if let Some(ports) = line_port {
-            // If more than one rpc server is started to the log will capture multiple ports
+            // If more than one rpc server is started the log will capture multiple ports
             // such as `addr=127.0.0.1:9944,[::1]:9944`
             let port_str: String = ports.chars().take_while(|c| c.is_numeric()).collect();
 
