f5ced8f3175d850783190ee44662908052a5021d
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-09-27 15:51:28 +0200

    chore(deps): bump a few dependencies

--- signer/src/crypto/secret_uri.rs
+++ signer/src/crypto/secret_uri.rs
@@ -108,10 +108,8 @@ impl core::str::FromStr for SecretUri {
         let password = cap.name("password");
 
         Ok(Self {
-            phrase: SecretString::from_str(phrase).expect("Returns infallible error; qed"),
-            password: password.map(|v| {
-                SecretString::from_str(v.as_str()).expect("Returns infallible error; qed")
-            }),
+            phrase: SecretString::from(phrase.to_owned()),
+            password: password.map(|v| SecretString::from(v.as_str().to_owned())),
             junctions,
         })
     }
