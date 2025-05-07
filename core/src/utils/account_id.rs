cd34332fa19f1c75c05f72cf29c7828c95531a71
James Wilson <james@jsdw.me>
2025-05-07 16:40:00 +0100

    Cargo update and bump substrate deps

--- core/src/utils/account_id.rs
+++ core/src/utils/account_id.rs
@@ -164,14 +164,14 @@ impl core::str::FromStr for AccountId32 {
 mod test {
     use super::*;
     use sp_core::{self, crypto::Ss58Codec};
-    use sp_keyring::AccountKeyring;
+    use sp_keyring::sr25519::Keyring;
 
     #[test]
     fn ss58_is_compatible_with_substrate_impl() {
         let keyrings = vec![
-            AccountKeyring::Alice,
-            AccountKeyring::Bob,
-            AccountKeyring::Charlie,
+            Keyring::Alice,
+            Keyring::Bob,
+            Keyring::Charlie,
         ];
 
         for keyring in keyrings {
