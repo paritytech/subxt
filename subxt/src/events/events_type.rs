42f1a62a0564d47cbb236f9d09885fa636cbf45c
James Wilson <james@jsdw.me>
2025-08-11 15:33:55 +0100

    Fix 1.89 clippy warnings (#2055)
    * Fix 1.89 clippy warnings
    
    * WASM lightclient clippy fixes too

--- subxt/src/events/events_type.rs
+++ subxt/src/events/events_type.rs
@@ -118,7 +118,7 @@ impl<T: Config> EventDetails<T> {
     }
 
     /// Fetch details from the metadata for this event.
-    pub fn event_metadata(&self) -> EventMetadataDetails {
+    pub fn event_metadata(&self) -> EventMetadataDetails<'_> {
         self.inner.event_metadata()
     }
 
