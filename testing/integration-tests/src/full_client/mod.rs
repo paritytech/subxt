fe79577bdce85bd00d3ed9330adaee9dec37d78a
dharjeezy <dharjeezy@gmail.com>
2025-08-12 16:50:46 +0100

    revert

---
+++ testing/integration-tests/src/full_client/mod.rs
@@ -0,0 +1,13 @@
+// Copyright 2019-2025 Parity Technologies (UK) Ltd.
+// This file is dual-licensed as Apache-2.0 or GPL-3.0.
+// see LICENSE for license details.
+
+mod blocks;
+mod client;
+mod codegen;
+mod frame;
+mod metadata_validation;
+mod pallet_view_functions;
+mod runtime_api;
+mod storage;
+mod transactions;
