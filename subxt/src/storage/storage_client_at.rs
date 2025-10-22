411506243b1601317fcb1d730d4331d8b257e3e7
James Wilson <james@jsdw.me>
2025-10-22 15:16:08 +0100

    fmt

--- subxt/src/storage/storage_client_at.rs
+++ subxt/src/storage/storage_client_at.rs
@@ -243,12 +243,12 @@ where
     Addr: Address,
     Client: OnlineClientT<T>,
 {
-    /// Fetch a storage value within this storage entry. 
-    /// 
+    /// Fetch a storage value within this storage entry.
+    ///
     /// This entry may be a map, and so you must provide the relevant values for each part of the storage
     /// key that is required in order to point to a single value.
-    /// 
-    /// If no value is found, the default value will be returned for this entry if one exists. If no value is 
+    ///
+    /// If no value is found, the default value will be returned for this entry if one exists. If no value is
     /// found and no default value exists, an error will be returned.
     pub async fn fetch(
         &self,
@@ -257,11 +257,11 @@ where
         fetch(&self.inner, &self.client, self.block_ref.hash(), key_parts).await
     }
 
-    /// Fetch a storage value within this storage entry. 
-    /// 
+    /// Fetch a storage value within this storage entry.
+    ///
     /// This entry may be a map, and so you must provide the relevant values for each part of the storage
     /// key that is required in order to point to a single value.
-    /// 
+    ///
     /// If no value is found, `None` will be returned.
     pub async fn try_fetch(
         &self,
@@ -271,10 +271,10 @@ where
     }
 
     /// Iterate over storage values within this storage entry.
-    /// 
-    /// You may provide any prefix of the values needed to point to a single value. Normally you will 
-    /// provide `()` to iterate over _everything_, or `(first_key,)` to iterate over everything underneath 
-    /// `first_key` in the map, or `(first_key, second_key)` to iterate over everything underneath `first_key` 
+    ///
+    /// You may provide any prefix of the values needed to point to a single value. Normally you will
+    /// provide `()` to iterate over _everything_, or `(first_key,)` to iterate over everything underneath
+    /// `first_key` in the map, or `(first_key, second_key)` to iterate over everything underneath `first_key`
     /// and `second_key` in the map, and so on, up to the actual depth of the map - 1.
     pub async fn iter<KeyParts: PrefixOf<Addr::KeyParts>>(
         &self,
