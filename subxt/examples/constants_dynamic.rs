ec2b61cd15b248ad4b272a56af574f3d1cee75c0
James Wilson <james@jsdw.me>
2025-10-28 15:35:40 +0000

    Show string tuple in constants example

--- subxt/examples/constants_dynamic.rs
+++ subxt/examples/constants_dynamic.rs
@@ -7,13 +7,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a client to use:
     let api = OnlineClient::<PolkadotConfig>::new().await?;
 
-    // A dynamic query to obtain some constant:
-    let constant_query = subxt::dynamic::constant::<Value>("System", "BlockLength");
+    // We can query a constant by providing a tuple of the pallet and constant name. The return type
+    // will be `Value` if we pass this query:
+    let constant_query = ("System", "BlockLength");
+    let _value = api.constants().at(&constant_query)?;
 
-    // Obtain the decoded constant:
+    // Or we can use the library function to query a constant, which allows us to pass a generic type
+    // that Subxt will attempt to decode the constant into:
+    let constant_query = subxt::dynamic::constant::<Value>("System", "BlockLength");
     let value = api.constants().at(&constant_query)?;
 
-    // Or obtain the bytes for the constant:
+    // Or we can obtain the bytes for the constant, using either form of query.
     let bytes = api.constants().bytes_at(&constant_query)?;
 
     println!("Constant bytes: {:?}", bytes);
