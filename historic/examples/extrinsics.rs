285dabc2f539f68c5ed675e7dcf09b8a372f7ab9
James Wilson <james@jsdw.me>
2025-11-21 12:18:19 +0000

    Add custom value decode to extrinsics example

--- historic/examples/extrinsics.rs
+++ historic/examples/extrinsics.rs
@@ -47,6 +47,9 @@ async fn main() -> Result<(), Box<dyn core::error::Error + Send + Sync + 'static
                     .visit(type_name::GetTypeName::new())?
                     .unwrap_or_default();
 
+                // When visiting fields we can also decode into a custom shape like so:
+                let _custom_value = field.visit(value::GetValue::new())?;
+
                 // We can also obtain and decode things without the complexity of the above:
                 println!(
                     "    {}: {} {}",
@@ -159,5 +162,308 @@ mod type_name {
         ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
             Ok(None)
         }
+    }
+}
+
+/// This visitor demonstrates how to decode and return a custom Value shape
+mod value {
+    use scale_decode::{
+        Visitor,
+        visitor::TypeIdFor,
+        visitor::types::{Array, BitSequence, Composite, Sequence, Str, Tuple, Variant},
+    };
+    use scale_info_legacy::LookupName;
+    use scale_type_resolver::TypeResolver;
+    use std::collections::HashMap;
+
+    /// A value type we're decoding into.
+    #[allow(dead_code)]
+    pub enum Value {
+        Number(f64),
+        BigNumber(String),
+        Bool(bool),
+        Char(char),
+        Array(Vec<Value>),
+        String(String),
+        Address(Vec<u8>),
+        I256([u8; 32]),
+        U256([u8; 32]),
+        Struct(HashMap<String, Value>),
+        Variant(String, VariantFields),
+    }
+
+    pub enum VariantFields {
+        Unnamed(Vec<Value>),
+        Named(HashMap<String, Value>),
+    }
+
+    /// An error we can encounter trying to decode things into a [`Value`]
+    #[derive(Debug, thiserror::Error)]
+    pub enum ValueError {
+        #[error("Decode error: {0}")]
+        Decode(#[from] scale_decode::visitor::DecodeError),
+        #[error("Cannot decode bit sequence: {0}")]
+        CannotDecodeBitSequence(codec::Error),
+    }
+
+    /// This is a visitor which obtains type names.
+    pub struct GetValue<R> {
+        marker: core::marker::PhantomData<R>,
+    }
+
+    impl<R> GetValue<R> {
+        /// Construct our TypeName visitor.
+        pub fn new() -> Self {
+            GetValue {
+                marker: core::marker::PhantomData,
+            }
+        }
+    }
+
+    impl<R> Visitor for GetValue<R>
+    where
+        R: TypeResolver,
+        R::TypeId: TryInto<LookupName>,
+    {
+        type Value<'scale, 'resolver> = Value;
+        type Error = ValueError;
+        type TypeResolver = R;
+
+        fn visit_i256<'resolver>(
+            self,
+            value: &[u8; 32],
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'_, 'resolver>, Self::Error> {
+            Ok(Value::I256(*value))
+        }
+
+        fn visit_u256<'resolver>(
+            self,
+            value: &[u8; 32],
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'_, 'resolver>, Self::Error> {
+            Ok(Value::U256(*value))
+        }
+
+        fn visit_i128<'scale, 'resolver>(
+            self,
+            value: i128,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            let attempt = value as f64;
+            if attempt as i128 == value {
+                Ok(Value::Number(attempt))
+            } else {
+                Ok(Value::BigNumber(value.to_string()))
+            }
+        }
+
+        fn visit_i64<'scale, 'resolver>(
+            self,
+            value: i64,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_i128(value.into(), type_id)
+        }
+
+        fn visit_i32<'scale, 'resolver>(
+            self,
+            value: i32,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_i128(value.into(), type_id)
+        }
+
+        fn visit_i16<'scale, 'resolver>(
+            self,
+            value: i16,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_i128(value.into(), type_id)
+        }
+
+        fn visit_i8<'scale, 'resolver>(
+            self,
+            value: i8,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_i128(value.into(), type_id)
+        }
+
+        fn visit_u128<'scale, 'resolver>(
+            self,
+            value: u128,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            let attempt = value as f64;
+            if attempt as u128 == value {
+                Ok(Value::Number(attempt))
+            } else {
+                Ok(Value::BigNumber(value.to_string()))
+            }
+        }
+
+        fn visit_u64<'scale, 'resolver>(
+            self,
+            value: u64,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_u128(value.into(), type_id)
+        }
+
+        fn visit_u32<'scale, 'resolver>(
+            self,
+            value: u32,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_u128(value.into(), type_id)
+        }
+
+        fn visit_u16<'scale, 'resolver>(
+            self,
+            value: u16,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_u128(value.into(), type_id)
+        }
+
+        fn visit_u8<'scale, 'resolver>(
+            self,
+            value: u8,
+            type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            self.visit_u128(value.into(), type_id)
+        }
+
+        fn visit_bool<'scale, 'resolver>(
+            self,
+            value: bool,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::Bool(value))
+        }
+
+        fn visit_char<'scale, 'resolver>(
+            self,
+            value: char,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::Char(value))
+        }
+
+        fn visit_array<'scale, 'resolver>(
+            self,
+            values: &mut Array<'scale, 'resolver, Self::TypeResolver>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::Array(to_array(values.remaining(), values)?))
+        }
+
+        fn visit_sequence<'scale, 'resolver>(
+            self,
+            values: &mut Sequence<'scale, 'resolver, Self::TypeResolver>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::Array(to_array(values.remaining(), values)?))
+        }
+
+        fn visit_str<'scale, 'resolver>(
+            self,
+            value: &mut Str<'scale>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::String(value.as_str()?.to_owned()))
+        }
+
+        fn visit_tuple<'scale, 'resolver>(
+            self,
+            values: &mut Tuple<'scale, 'resolver, Self::TypeResolver>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            Ok(Value::Array(to_array(values.remaining(), values)?))
+        }
+
+        fn visit_bitsequence<'scale, 'resolver>(
+            self,
+            value: &mut BitSequence<'scale>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            let bits = value.decode()?;
+            let mut out = Vec::with_capacity(bits.len());
+            for b in bits {
+                let b = b.map_err(ValueError::CannotDecodeBitSequence)?;
+                out.push(Value::Bool(b));
+            }
+            Ok(Value::Array(out))
+        }
+
+        fn visit_composite<'scale, 'resolver>(
+            self,
+            value: &mut Composite<'scale, 'resolver, Self::TypeResolver>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            // Special case for ss58 addresses:
+            if let Some(n) = value.name()
+                && n == "AccountId32"
+                && value.bytes_from_start().len() == 32
+            {
+                return Ok(Value::Address(value.bytes_from_start().to_vec()));
+            } else {
+            }
+
+            // Reuse logic for decoding variant fields:
+            match to_variant_fieldish(value)? {
+                VariantFields::Named(s) => Ok(Value::Struct(s)),
+                VariantFields::Unnamed(a) => Ok(Value::Array(a)),
+            }
+        }
+
+        fn visit_variant<'scale, 'resolver>(
+            self,
+            value: &mut Variant<'scale, 'resolver, Self::TypeResolver>,
+            _type_id: TypeIdFor<Self>,
+        ) -> Result<Self::Value<'scale, 'resolver>, Self::Error> {
+            let name = value.name().to_owned();
+            let fields = to_variant_fieldish(value.fields())?;
+            Ok(Value::Variant(name, fields))
+        }
+    }
+
+    fn to_variant_fieldish<'scale, 'resolver, R>(
+        value: &mut Composite<'scale, 'resolver, R>,
+    ) -> Result<VariantFields, ValueError>
+    where
+        R: TypeResolver,
+        R::TypeId: TryInto<LookupName>,
+    {
+        // If fields are unnamed, treat as array:
+        if value.fields().iter().all(|f| f.name.is_none()) {
+            return Ok(VariantFields::Unnamed(to_array(value.remaining(), value)?));
+        }
+
+        // Otherwise object:
+        let mut out = HashMap::new();
+        for field in value {
+            let field = field?;
+            let name = field.name().unwrap().to_string();
+            let value = field.decode_with_visitor(GetValue::new())?;
+            out.insert(name, value);
+        }
+        Ok(VariantFields::Named(out))
+    }
+
+    fn to_array<'scale, 'resolver, R>(
+        len: usize,
+        mut values: impl scale_decode::visitor::DecodeItemIterator<'scale, 'resolver, R>,
+    ) -> Result<Vec<Value>, ValueError>
+    where
+        R: TypeResolver,
+        R::TypeId: TryInto<LookupName>,
+    {
+        let mut out = Vec::with_capacity(len);
+        while let Some(value) = values.decode_item(GetValue::new()) {
+            out.push(value?);
+        }
+        Ok(out)
     }
 }
