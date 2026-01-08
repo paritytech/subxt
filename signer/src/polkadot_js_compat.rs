c5cf5ff70d62630ab9cadc957bc27f7649da2bf2
James Wilson <james@jsdw.me>
2026-01-08 10:44:53 +0000

    Actually fix polkadot-js-compat thing

--- signer/src/polkadot_js_compat.rs
+++ signer/src/polkadot_js_compat.rs
@@ -156,7 +156,8 @@ impl KeyringPairJson {
         let keypair = sr25519::Keypair::from_ed25519_bytes(secret_key)?;
 
         // Ensure keys are correct.
-        if keypair.public_key().0 != public_key || AccountId32(keypair.public_key()) != self.address
+        if keypair.public_key().0 != public_key
+            || AccountId32(keypair.public_key().0) != self.address
         {
             return Err(Error::InvalidKeys);
         }
