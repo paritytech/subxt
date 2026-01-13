fe78bd01ad06c9a77006362662994340396c6aa9
wmjae <wenmujia@gmail.com>
2026-01-13 02:17:36 +0000

    update copyright year to 2026

--- testing/generate-custom-metadata/src/lib.rs
+++ testing/generate-custom-metadata/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2025 Parity Technologies (UK) Ltd.
+// Copyright 2019-2026 Parity Technologies (UK) Ltd.
 // This file is dual-licensed as Apache-2.0 or GPL-3.0.
 // see LICENSE for license details.
 
